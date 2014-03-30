d73aebb15e8db1b656159e7f6b0a24c9d9ce6c27
Miguel Angel Torres Suarez <miguelangel.torres@outlook.com>
2014-03-30 23:41:05 +0200

    First commit, home.

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+#Datos Zona Horario
+date_default_timezone_set('Europe/Madrid');
+$mashours=0;   //apartir 27.3.2011
+
+# Datos MySql
+$dbhost = "localhost";
+$dbuname= "root" ;
+$dbpass = "";
+$dbname = "pes";
+
+
+# Datos Generales del Sitio
+define('_SITE_URL', 'www.estadisticamadrid.lldm'); // URL del Sitio (sin /)
+
+# Datos Cliente
+define('_EMP_TEL',	'91 123 45 67');
+define('_EMP_MOTO',	'Su Empresa, S.L.  Mensajeros rapidos');
+define('_MIN_TITLE', 'Ministerio de Estadística - La Luz del Mundo Madrid');
+define('_EMP_MAILTO', '<a href="mailto:comercial@pes-systems.net">comercial@pes-systems.net</a>' );
+define('_MIN_TYPE',	'Ministerio de Estadística');
+define('_MIN_COPYRIGHT', 'Ministerío de Estadística © La Luz del Mundo 2014');
+
+ $mailreco_to = "pruebas@pes-systems.net";
+ $mailreco_from = "pruebas@pes-systems.net";
+// $mailreco_subject="Solicitude de recolección desde la web ".$rnumserv ." Hr ".$horatoma;
+ //$mailreco_body = $msg; 
+ $mail_host     = "mail.recogidas.net";
+ $mail_username = "clientes@recogidas.net";
+ $mail_password = "1234567890";
+ $mail_port = "587"; 
+
+
+
